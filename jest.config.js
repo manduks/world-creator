7c9e4257652d3c3430b5a8b78f62705e95942d26
Armando Gonzalez <iam@armando.mx>
2020-12-13 22:31:56 -0600

    tests setup

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^components(.*)$': '<rootDir>/components$1',
+    '^context(.*)$': '<rootDir>/context$1',
+    '^styles(.*)$': '<rootDir>/styles$1',
+    '^lib(.*)$': '<rootDir>/lib$1',
+  },
+}
